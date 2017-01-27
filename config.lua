874b82330eabca74b0158e1bbb8ec0d131b16782
rickarof <kryonz_ricardo@hotmail.com>
2017-01-27 19:56:03 -0200

    compilado

---
+++ config.lua
@@ -0,0 +1,104 @@
+-- Combat settings
+-- NOTE: valid values for worldType are: "pvp", "no-pvp" and "pvp-enforced"
+worldType = "pvp"
+hotkeyAimbotEnabled = true
+protectionLevel = 1
+killsToRedSkull = 3
+killsToBlackSkull = 6
+pzLocked = 60000
+removeChargesFromRunes = true
+timeToDecreaseFrags = 24 * 60 * 60 * 1000
+whiteSkullTime = 15 * 60 * 1000
+stairJumpExhaustion = 2000
+experienceByKillingPlayers = false
+expFromPlayersLevelRange = 75
+
+-- Connection Config
+-- NOTE: maxPlayers set to 0 means no limit
+ip = "192.168.0.100"
+bindOnlyGlobalAddress = false
+loginProtocolPort = 7171
+gameProtocolPort = 7172
+statusProtocolPort = 7171
+maxPlayers = 0
+motd = "Seja bem-vindo ao Magestibia!"
+onePlayerOnlinePerAccount = true
+allowClones = false
+serverName = "Magestibia"
+statusTimeout = 5000
+replaceKickOnLogin = true
+maxPacketsPerSecond = 25
+
+-- Deaths
+-- NOTE: Leave deathLosePercent as -1 if you want to use the default
+-- death penalty formula. For the old formula, set it to 10. For
+-- no skill/experience loss, set it to 0.
+deathLosePercent = -1
+
+-- Houses
+-- NOTE: set housePriceEachSQM to -1 to disable the ingame buy house functionality
+housePriceEachSQM = 1000
+houseRentPeriod = "never"
+
+-- Item Usage
+timeBetweenActions = 200
+timeBetweenExActions = 1000
+
+-- Map
+-- NOTE: set mapName WITHOUT .otbm at the end
+mapName = "mapa"
+mapAuthor = "Komic"
+
+-- Market
+marketOfferDuration = 30 * 24 * 60 * 60
+premiumToCreateMarketOffer = true
+checkExpiredMarketOffersEachMinutes = 60
+maxMarketOffersAtATimePerPlayer = 100
+
+-- MySQL
+mysqlHost = "localhost"
+mysqlUser = "root"
+mysqlPass = ""
+mysqlDatabase = "premium"
+mysqlPort = 3306
+mysqlSock = ""
+passwordType = "sha1"
+
+-- Misc.
+allowChangeOutfit = true
+freePremium = false
+kickIdlePlayerAfterMinutes = 15
+maxMessageBuffer = 4
+emoteSpells = false
+classicEquipmentSlots = false
+
+-- Rates
+-- NOTE: rateExp is not used if you have enabled stages in data/XML/stages.xml
+rateExp = 600
+rateSkill = 50
+rateLoot = 5
+rateMagic = 30
+rateSpawn = 1
+
+-- Monsters
+deSpawnRange = 2
+deSpawnRadius = 50
+
+-- Stamina
+staminaSystem = true
+
+-- Scripts
+warnUnsafeScripts = true
+convertUnsafeScripts = true
+
+-- Startup
+-- NOTE: defaultPriority only works on Windows and sets process
+-- priority, valid values are: "normal", "above-normal", "high"
+defaultPriority = "high"
+startupDatabaseOptimization = false
+
+-- Status server information
+ownerName = ""
+ownerEmail = ""
+url = "https://otland.net/"
+location = "Sweden"
